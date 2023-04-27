9c894dbc15eca5190ab4d3ae6dc0e05203a7c9c4
kapr <kyowon1108@gmail.com>
2023-04-28 00:41:51 +0900

    asdf

--- src/main/java/com/example/swproject/controller/Hello.java
+++ src/main/java/com/example/swproject/controller/Hello.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
-
     @GetMapping("/test")
     public String test() {
         return "Hello World!";
